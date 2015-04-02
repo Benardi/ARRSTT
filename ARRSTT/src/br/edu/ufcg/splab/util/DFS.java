a278ad0d426541b65d371a60ae70ff0c94410a12
ArthurEmanuelRodriguesCosta <arthur.costa@ccc.ufcg.edu.br>
2015-04-01 21:52:09 -0300

    DFS algorithm

--- ARRSTT/src/br/edu/ufcg/splab/util/DFS.java
+++ ARRSTT/src/br/edu/ufcg/splab/util/DFS.java
@@ -1,5 +1,18 @@
 package br.edu.ufcg.splab.util;
 
-public class DFS {
+import java.util.List;
 
+import br.edu.ufcg.splab.core.graph.Graph;
+import br.edu.ufcg.splab.core.vertex.Vertex;
+
+public class DFS {
+	private final Graph graph;
+	
+	public DFS(Graph graph) {
+		this.graph = graph;
+	}
+	
+	public List<List<Vertex>> search() {
+		
+	}
 }
