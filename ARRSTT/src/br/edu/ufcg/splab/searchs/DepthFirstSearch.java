ff6452841fabe730c2fbad2aaa12390c496d4a6b
ArthurEmanuelRodriguesCosta <arthur.costa@ccc.ufcg.edu.br>
2015-04-08 01:27:33 -0300

    Depth-First Search

--- ARRSTT/src/br/edu/ufcg/splab/searchs/DepthFirstSearch.java
+++ ARRSTT/src/br/edu/ufcg/splab/searchs/DepthFirstSearch.java
@@ -4,34 +4,49 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.edu.ufcg.splab.core.InterfaceEdge;
+import br.edu.ufcg.splab.core.InterfaceGraph;
 import br.edu.ufcg.splab.core.InterfaceVertex;
-import br.edu.ufcg.splab.core.graph.Graph;
+import br.edu.ufcg.splab.parser.ReadTGF;
 
 public class DepthFirstSearch {
-	private final Graph graph;
-	
-	public DepthFirstSearch(Graph graph) {
-		this.graph = graph;
-	}
+	List<InterfaceEdge> visited = new ArrayList<InterfaceEdge>();
+	List<InterfaceEdge> testCase = new ArrayList<InterfaceEdge>();
+	List<List<InterfaceEdge>> paths = new ArrayList<List<InterfaceEdge>>();
 	
 	public List<List<InterfaceEdge>> search(InterfaceVertex vertex) {
-		List<InterfaceEdge> visited = new ArrayList<InterfaceEdge>();
-		List<List<InterfaceEdge>> paths = new ArrayList<List<InterfaceEdge>>();
-		
-		if(vertex.isLeaf()) { 
-			paths.add(visited);
-			visited.remove(visited.size() - 1); // I hope it's not a gambiarra
+		if(vertex.isLeaf()) {  
+			paths.add(testCase);
 		}
 		
 		for (InterfaceEdge edge : vertex.getOutTransitions()) {
 			if(!visited.contains(edge)) {
 				visited.add(edge);
+				testCase.add(edge);
 				search(edge.getTo());
+				testCase.remove(testCase.size() - 1);
 			}
 		}
-		
+
 		return paths;
 	}
 	
+	public static void main(String[] args) { // Just for testing.
+		ReadTGF tgfReader = new ReadTGF();
+		try {
+			InterfaceGraph graph = tgfReader.getGraph("input_examples/simple_example.tgf");
+			DepthFirstSearch searchObject = new DepthFirstSearch();
+			List<List<InterfaceEdge>> paths = searchObject.search(graph.getRoot());
+			/*
+			for (List<InterfaceEdge> path: paths) {
+				for(InterfaceEdge e : path) {
+					System.out.print(e + "  ");
+				}
+				System.out.println();
+				System.out.println("=====================");
+			}*/
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
 	
 }
