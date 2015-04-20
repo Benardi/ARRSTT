70e0bd701a139dc0af78ee954841b89a922d0e17
Iaron <iaron.araujo@ccc.ufcg.edu.br>
2015-04-20 15:18:39 -0300

    DFS "working" for loops (has to be checked).

--- ARRSTT/src/br/edu/ufcg/splab/searchs/DepthFirstSearch.java
+++ ARRSTT/src/br/edu/ufcg/splab/searchs/DepthFirstSearch.java
@@ -1,6 +1,7 @@
 package br.edu.ufcg.splab.searchs;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Stack;
 
@@ -11,20 +12,42 @@ import br.edu.ufcg.splab.parser.ReadTGF;
 import br.edu.ufcg.splab.util.TestCase;
 
 public class DepthFirstSearch {
+	private static HashMap<InterfaceEdge, Integer> vertexCoverage;  // TIRA STATIC
 	
-	public List<TestCase> getTestSuite(InterfaceVertex root) {
-		return search(root, new Stack<InterfaceEdge>(), new ArrayList<TestCase>());
+	public DepthFirstSearch() {
+		vertexCoverage = new HashMap<InterfaceEdge, Integer>();
 	}
 	
-	private List<TestCase> search(InterfaceVertex vertex, Stack<InterfaceEdge> tCase, List<TestCase> testSuite) {
+	public List<TestCase> getTestSuite(InterfaceVertex root, Integer loopCoverage) {
+		
+		return search(root, new Stack<InterfaceEdge>(), new ArrayList<TestCase>(), loopCoverage);
+	}
+	
+	private List<TestCase> search(InterfaceVertex vertex, Stack<InterfaceEdge> tCase, List<TestCase> testSuite, Integer loopCoverage) {
 		if (vertex.isLeaf() && !testSuite.contains(tCase)) {  
 			testSuite.add(new TestCase(new ArrayList<InterfaceEdge>(tCase)));
 		}
 		
 		for (InterfaceEdge edge : vertex.getOutTransitions()) {
+			
+			if(!(vertexCoverage.containsKey(edge))){
+				vertexCoverage.put(edge, 0);
+			}
+			
+			if (vertexCoverage.get(edge) < loopCoverage) {	
+				if (tCase.contains(edge)){
+					Integer value = vertexCoverage.get(edge);
+					vertexCoverage.put(edge, value + 1);
+				}
+				
 				tCase.push(edge);
-				search(edge.getTo(), tCase, testSuite);
+				search(edge.getTo(), tCase, testSuite, loopCoverage);
 				tCase.pop();
+			}
+			/*
+			tCase.push(edge);
+			search(edge.getTo(), tCase, testSuite, loopCoverage);
+			tCase.pop();*/
 		}
 		
 		return testSuite;
@@ -33,10 +56,10 @@ public class DepthFirstSearch {
 	public static void main(String[] args) { // Just for testing.
 		ReadTGF tgfReader = new ReadTGF();
 		try {
-			InterfaceGraph graph = tgfReader.getGraph("put_path_here.tgf");
+			InterfaceGraph graph = tgfReader.getGraph("C:\\Users\\Iaron\\git\\Application-of-Reproducibility-Research-with-Software-Testing-Techniques\\ARRSTT\\input_examples\\littlelittletoy.tgf");
 			DepthFirstSearch searchObject = new DepthFirstSearch();
 			long time = System.currentTimeMillis();
-			List<TestCase> paths = searchObject.getTestSuite(graph.getRoot());
+			List<TestCase> paths = searchObject.getTestSuite(graph.getRoot(), 1);
 			System.out.println(System.currentTimeMillis() - time);
 			for (List<InterfaceEdge> path: paths) {
 				for(InterfaceEdge e : path) {
@@ -45,6 +68,9 @@ public class DepthFirstSearch {
 				System.out.println();
 				System.out.println("=====================");
 			}
+			
+			System.out.println(vertexCoverage);
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
--- ARRSTT/src/br/edu/ufcg/splab/tests/TestDepthFirstSearch.java
+++ ARRSTT/src/br/edu/ufcg/splab/tests/TestDepthFirstSearch.java
@@ -24,69 +24,69 @@ public class TestDepthFirstSearch {
 		// Easy toys
 		
 		graph = tgfReader.getGraph("/ARRSTT/input_examples/iaron_easytoy1.tgf");
-		cases = searchObject.search(graph.getRoot());
+		cases = searchObject.getTestSuite(graph.getRoot());
 		Assert.assertEquals(2, cases.size());
 		
 		graph = tgfReader.getGraph("/ARRSTT/input_examples/iaron_easytoy2.tgf");
-		cases = searchObject.search(graph.getRoot());
+		cases = searchObject.getTestSuite(graph.getRoot());
 		Assert.assertEquals(2, cases.size());
 		
 		graph = tgfReader.getGraph("/ARRSTT/input_examples/iaron_easytoy3.tgf");
-		cases = searchObject.search(graph.getRoot());
+		cases = searchObject.getTestSuite(graph.getRoot());
 		Assert.assertEquals(6, cases.size());
 		
 		graph = tgfReader.getGraph("/ARRSTT/input_examples/iaron_easytoy4.tgf");
-		cases = searchObject.search(graph.getRoot());
+		cases = searchObject.getTestSuite(graph.getRoot());
 		Assert.assertEquals(4, cases.size());
 		
 		graph = tgfReader.getGraph("/ARRSTT/input_examples/iaron_easytoy5.tgf");
-		cases = searchObject.search(graph.getRoot());
+		cases = searchObject.getTestSuite(graph.getRoot());
 		Assert.assertEquals(2, cases.size());
 		
 		// Medium toys
 		
 		graph = tgfReader.getGraph("/ARRSTT/input_examples/iaron_mediumtoy1.tgf");
-		cases = searchObject.search(graph.getRoot());
+		cases = searchObject.getTestSuite(graph.getRoot());
 		Assert.assertEquals(4, cases.size());
 		
 		graph = tgfReader.getGraph("/ARRSTT/input_examples/iaron_mediumtoy2.tgf");
-		cases = searchObject.search(graph.getRoot());
+		cases = searchObject.getTestSuite(graph.getRoot());
 		Assert.assertEquals(4, cases.size());
 		
 		graph = tgfReader.getGraph("/ARRSTT/input_examples/iaron_mediumtoy3.tgf");
-		cases = searchObject.search(graph.getRoot());
+		cases = searchObject.getTestSuite(graph.getRoot());
 		Assert.assertEquals(10, cases.size());
 		
 		graph = tgfReader.getGraph("/ARRSTT/input_examples/iaron_mediumtoy4.tgf");
-		cases = searchObject.search(graph.getRoot());
+		cases = searchObject.getTestSuite(graph.getRoot());
 		Assert.assertEquals(7, cases.size());
 		
 		// Difficult toys
 		
 		graph = tgfReader.getGraph("/ARRSTT/input_examples/iaron_difficulttoy1.tgf");
-		cases = searchObject.search(graph.getRoot());
+		cases = searchObject.getTestSuite(graph.getRoot());
 		Assert.assertEquals(4, cases.size());
 		
 		graph = tgfReader.getGraph("/ARRSTT/input_examples/iaron_difficulttoy2.tgf");
-		cases = searchObject.search(graph.getRoot());
+		cases = searchObject.getTestSuite(graph.getRoot());
 		Assert.assertEquals(6, cases.size());
 		
 		graph = tgfReader.getGraph("/ARRSTT/input_examples/iaron_difficulttoy3.tgf");
-		cases = searchObject.search(graph.getRoot());
+		cases = searchObject.getTestSuite(graph.getRoot());
 		Assert.assertEquals(10, cases.size());
 		
 		graph = tgfReader.getGraph("/ARRSTT/input_examples/iaron_difficulttoy4.tgf");
-		cases = searchObject.search(graph.getRoot());
+		cases = searchObject.getTestSuite(graph.getRoot());
 		Assert.assertEquals(12, cases.size());
 		
 		// Real graphs
 		
 		graph = tgfReader.getGraph("/ARRSTT/input_examples/jo_ken_po_exemplo.tgf");
-		cases = searchObject.search(graph.getRoot());
+		cases = searchObject.getTestSuite(graph.getRoot());
 		Assert.assertEquals(9, cases.size());
 		
 		graph = tgfReader.getGraph("/ARRSTT/input_examples/Dark_Souls_2_Chest.tgf");
-		cases = searchObject.search(graph.getRoot());
+		cases = searchObject.getTestSuite(graph.getRoot());
 		Assert.assertEquals(7, cases.size());
 	}
 
