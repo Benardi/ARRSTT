3c26084b54df085709be4bc26e58d4c6da56e638
Iaron <iaron.araujo@ccc.ufcg.edu.br>
2015-04-21 11:24:36 -0300

    DFS Working for some small tests I made. Has to be checked for bigger and more complex graphs, and maybe the concept of loop coverage have to be checked.

--- ARRSTT/src/br/edu/ufcg/splab/searchs/DepthFirstSearch.java
+++ ARRSTT/src/br/edu/ufcg/splab/searchs/DepthFirstSearch.java
@@ -12,15 +12,15 @@ import br.edu.ufcg.splab.parser.ReadTGF;
 import br.edu.ufcg.splab.util.TestCase;
 
 public class DepthFirstSearch {
-	private static HashMap<InterfaceEdge, Integer> vertexCoverage;  // TIRA STATIC
+	private static HashMap<InterfaceEdge, Integer> vertexCoverage;  // TIRAR STATIC
 	
 	public DepthFirstSearch() {
 		vertexCoverage = new HashMap<InterfaceEdge, Integer>();
 	}
 	
 	public List<TestCase> getTestSuite(InterfaceVertex root, Integer loopCoverage) {
-		
 		return search(root, new Stack<InterfaceEdge>(), new ArrayList<TestCase>(), loopCoverage);
+		// Lembrar de limpar o Mapa
 	}
 	
 	private List<TestCase> search(InterfaceVertex vertex, Stack<InterfaceEdge> tCase, List<TestCase> testSuite, Integer loopCoverage) {
@@ -34,20 +34,17 @@ public class DepthFirstSearch {
 				vertexCoverage.put(edge, 0);
 			}
 			
-			if (vertexCoverage.get(edge) < loopCoverage) {	
-				if (tCase.contains(edge)){
-					Integer value = vertexCoverage.get(edge);
-					vertexCoverage.put(edge, value + 1);
-				}
-				
-				tCase.push(edge);
-				search(edge.getTo(), tCase, testSuite, loopCoverage);
-				tCase.pop();
-			}
-			/*
+			Integer value = vertexCoverage.get(edge);
+			
 			tCase.push(edge);
-			search(edge.getTo(), tCase, testSuite, loopCoverage);
-			tCase.pop();*/
+				
+			if (vertexCoverage.get(edge) != loopCoverage){
+				vertexCoverage.put(edge, value + 1);
+				search(edge.getTo(), tCase, testSuite, loopCoverage);
+			}
+			
+			tCase.pop();
+			vertexCoverage.put(edge, value - 1);
 		}
 		
 		return testSuite;
@@ -59,7 +56,7 @@ public class DepthFirstSearch {
 			InterfaceGraph graph = tgfReader.getGraph("C:\\Users\\Iaron\\git\\Application-of-Reproducibility-Research-with-Software-Testing-Techniques\\ARRSTT\\input_examples\\littlelittletoy.tgf");
 			DepthFirstSearch searchObject = new DepthFirstSearch();
 			long time = System.currentTimeMillis();
-			List<TestCase> paths = searchObject.getTestSuite(graph.getRoot(), 1);
+			List<TestCase> paths = searchObject.getTestSuite(graph.getRoot(), 2);
 			System.out.println(System.currentTimeMillis() - time);
 			for (List<InterfaceEdge> path: paths) {
 				for(InterfaceEdge e : path) {
