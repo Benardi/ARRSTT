f7496f89f27482232ed6af52b44f866b63a2a519
SILVAWesley <wesley.silva@ccc.ufcg.edu.br>
2015-04-23 19:12:26 -0300

    BFS working for some graphs with loops.

--- ARRSTT/src/br/edu/ufcg/splab/searchs/BreadthFirstSearch.java
+++ ARRSTT/src/br/edu/ufcg/splab/searchs/BreadthFirstSearch.java
@@ -1,5 +1,8 @@
 package br.edu.ufcg.splab.searchs;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -7,6 +10,7 @@ import java.util.Queue;
 import br.edu.ufcg.splab.core.InterfaceEdge;
 import br.edu.ufcg.splab.core.InterfaceGraph;
 import br.edu.ufcg.splab.core.InterfaceVertex;
+import br.edu.ufcg.splab.core.edges.Edge;
 import br.edu.ufcg.splab.parser.ReadTGF;
 import br.edu.ufcg.splab.util.TestCase;
 
@@ -14,10 +18,13 @@ public class BreadthFirstSearch {
 	public static void main(String[] args) {
 		try {
 			ReadTGF tgfReader = new ReadTGF();
-			InterfaceGraph graph = tgfReader.getGraph("C:\\Users\\Wesley\\workspace\\Application-of-Reproducibility-Research-with-Software-Testing-Techniques-master\\ARRSTT\\input_examples\\iaron_difficulttoy3.tgf");
-			NewBreadthFirstSearch searchObject = new NewBreadthFirstSearch();
+			InterfaceGraph graph = tgfReader.getGraph("put_path_here.tgf");
+			BreadthFirstSearch searchObject = new BreadthFirstSearch();
 			
-			for (List<InterfaceEdge> testCase: searchObject.getTestSuite(graph.getRoot())) {
+			long time = System.currentTimeMillis();
+			searchObject.getTestSuite(graph.getRoot(), 1);
+			System.out.println(System.currentTimeMillis() - time);
+			for (List<InterfaceEdge> testCase: searchObject.getTestSuite(graph.getRoot(), 1)) {
 				for(InterfaceEdge e : testCase) {
 					System.out.print(e + "  ");
 				}
@@ -29,57 +36,107 @@ public class BreadthFirstSearch {
 		}
 			
 	}	
-
-	private Queue<InterfaceEdge> queue;
 	
-	public List<TestCase> getTestSuite(InterfaceVertex root) {
-		queue = new LinkedList<InterfaceEdge>();
-		return search(root, initializeTestSuite(root));
+	private int nLoopCoverage;
+	private List<InterfaceEdge> notToVisitEdges;
+	
+	public List<TestCase> getTestSuite(InterfaceVertex root, Integer nLoopCoverage) {
+	   return search(root, nLoopCoverage);
 	}
-	
-	private List<TestCase> search(InterfaceVertex vertex, List<TestCase> testSuite) {
-		dequeue(vertex);
-		divideTestSuite(vertex, testSuite);
-		if (!queue.isEmpty()) return search(queue.peek().getTo(), testSuite); // BASE CASE
-		return testSuite;
-	}
-	
-	private void dequeue(InterfaceVertex vertex) {
-		queue.poll();
-		if (!vertex.isLeaf()) {
-			queue.addAll(vertex.getOutTransitions());	
-		}
-	}
-	
-	private List<TestCase> divideTestSuite(InterfaceVertex vertex, List<TestCase> testSuite) {
-		int loopEnd = testSuite.size();
-		List<TestCase> uselessTestCases = new LinkedList<TestCase>();
+	    
+	private List<TestCase> search(InterfaceVertex root, Integer nLoopCoverage) {
+		this.notToVisitEdges = new ArrayList<InterfaceEdge>();
+		this.nLoopCoverage = nLoopCoverage;
 		
-		for (InterfaceEdge e : vertex.getOutTransitions()) {
-			for (InterfaceEdge eg: vertex.getInTransitions()) {
-				for (int i = 0; i < loopEnd; i++) {
-					if (testSuite.get(i).get(testSuite.get(i).size() - 1).equals(eg)) {
-						TestCase newTestCase = new TestCase(testSuite.get(i));
-						newTestCase.add(e);
-						testSuite.add(newTestCase);
-						uselessTestCases.add(testSuite.get(i));
-					}
+		List<TestCase> testSuite = initializeTestSuite(root);
+		Queue<InterfaceEdge> queue = new LinkedList<InterfaceEdge>();
+		queue.addAll(root.getOutTransitions());
+	        
+		while (!queue.isEmpty()) { 
+			divideTestSuite(dequeue(queue), testSuite);
+		}
+	    
+		cleanTestSuite(testSuite);
+	    return testSuite;
+	}
+	    
+	private InterfaceEdge dequeue(Queue<InterfaceEdge> queue) {
+		InterfaceEdge actualEdge = queue.poll();
+	            
+		if (!actualEdge.getTo().isLeaf() && !notToVisitEdges.contains(actualEdge)) {
+			queue.addAll(actualEdge.getTo().getOutTransitions());
+		}
+	        
+		return actualEdge;
+	}
+	    
+	private void divideTestSuite(InterfaceEdge actualEdge, List<TestCase> testSuite) {
+		int loopEnd = testSuite.size();
+		List<TestCase> uselessTestCases = new ArrayList<TestCase>(); 
+		
+		for (InterfaceEdge e : actualEdge.getTo().getOutTransitions()) {
+			for (int i = 0; i < loopEnd; i++) {
+				if (testSuite.get(i).get(testSuite.get(i).size() - 1).equals(actualEdge) && !notToVisitEdges.contains(e)) {
+					TestCase newTestCase = new TestCase(testSuite.get(i));
+					newTestCase.add(e);
+					testSuite.add(newTestCase);
+					takeOut(newTestCase, testSuite);
+					uselessTestCases.add(testSuite.get(i));
 				}
 			}
 		}
-		
+			
 		testSuite.removeAll(uselessTestCases);
-		return testSuite;
+	}
+	
+	private void takeOut(TestCase testCase, List<TestCase> testSuite) {
+		HashMap<InterfaceEdge, Integer> coverageMap = new HashMap<InterfaceEdge, Integer>();
+		
+		for (InterfaceEdge edge : testCase) {
+			if (!coverageMap.containsKey(edge)) {
+				coverageMap.put(edge, 0);
+			} else {
+				coverageMap.put(edge, coverageMap.get(edge) + 1);
+			}
+			
+			if (coverageMap.get(edge) >= nLoopCoverage && elementFrequency(testCase, testCase.get(testCase.size() - 2)) == 1 && testCase.size() > 1) {
+				notToVisitEdges.add(testCase.get(testCase.size() - 2));
+				break;
+			}
+ 		}
+	}
+	
+	private int elementFrequency(TestCase testCase, InterfaceEdge edge) {
+		int frequency = 0;
+		
+		for (InterfaceEdge e : testCase) {
+			if (edge.equals(e)) {
+				frequency++;
+			}
+		}
+		
+		return frequency;
+	}
+	
+	private void cleanTestSuite(List<TestCase> testSuite) {
+		List<TestCase> testSuiteCopy = new ArrayList<TestCase>(testSuite);
+		
+		for (TestCase e : testSuiteCopy) {
+			if (!e.get(e.size() - 1).getTo().isLeaf()) {
+				testSuite.remove(e);
+			}
+		}
 	}
 	
 	private List<TestCase> initializeTestSuite(InterfaceVertex root) {
-		List<TestCase> testSuite = new LinkedList<TestCase>();
-		for(InterfaceEdge edge : root.getOutTransitions()) {
-			TestCase tc = new TestCase();
-			tc.add(edge);
-			testSuite.add(tc);
+		List<TestCase> testSuite = new ArrayList<TestCase>();
+	    	
+		for (InterfaceEdge i : root.getOutTransitions()) {
+			TestCase tempTestCase = new TestCase();
+			tempTestCase.add(i);
+			testSuite.add(tempTestCase);
 		}
-		
-		return testSuite; 
+			
+		return testSuite;
 	}
 }
