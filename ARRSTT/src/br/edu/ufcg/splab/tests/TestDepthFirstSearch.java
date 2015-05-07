6da71118182198f5c2c382150d0d6d4f61720d13
Iaron <iaron.araujo@ccc.ufcg.edu.br>
2015-05-07 19:22:24 -0300

    Deleted that very old Main I created, removed a small warning on the Graph class, fixed the TestDFS and created with Wesley's help the Experiment1 class, that put the Execution time of a Search in a txt

--- ARRSTT/src/br/edu/ufcg/splab/tests/TestDepthFirstSearch.java
+++ ARRSTT/src/br/edu/ufcg/splab/tests/TestDepthFirstSearch.java
@@ -3,7 +3,6 @@ package br.edu.ufcg.splab.tests;
 import java.util.List;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 import br.edu.ufcg.splab.core.InterfaceGraph;
@@ -24,69 +23,69 @@ public class TestDepthFirstSearch {
 		// Easy toys
 		
 		graph = tgfReader.getGraph("/ARRSTT/input_examples/iaron_easytoy1.tgf");
-		cases = searchObject.getTestSuite(graph.getRoot());
+		cases = searchObject.getTestSuite(graph.getRoot(), 0);
 		Assert.assertEquals(2, cases.size());
 		
 		graph = tgfReader.getGraph("/ARRSTT/input_examples/iaron_easytoy2.tgf");
-		cases = searchObject.getTestSuite(graph.getRoot());
+		cases = searchObject.getTestSuite(graph.getRoot(), 0);
 		Assert.assertEquals(2, cases.size());
 		
 		graph = tgfReader.getGraph("/ARRSTT/input_examples/iaron_easytoy3.tgf");
-		cases = searchObject.getTestSuite(graph.getRoot());
+		cases = searchObject.getTestSuite(graph.getRoot(), 0);
 		Assert.assertEquals(6, cases.size());
 		
 		graph = tgfReader.getGraph("/ARRSTT/input_examples/iaron_easytoy4.tgf");
-		cases = searchObject.getTestSuite(graph.getRoot());
+		cases = searchObject.getTestSuite(graph.getRoot(), 0);
 		Assert.assertEquals(4, cases.size());
 		
 		graph = tgfReader.getGraph("/ARRSTT/input_examples/iaron_easytoy5.tgf");
-		cases = searchObject.getTestSuite(graph.getRoot());
+		cases = searchObject.getTestSuite(graph.getRoot(), 0);
 		Assert.assertEquals(2, cases.size());
 		
 		// Medium toys
 		
 		graph = tgfReader.getGraph("/ARRSTT/input_examples/iaron_mediumtoy1.tgf");
-		cases = searchObject.getTestSuite(graph.getRoot());
+		cases = searchObject.getTestSuite(graph.getRoot(), 0);
 		Assert.assertEquals(4, cases.size());
 		
 		graph = tgfReader.getGraph("/ARRSTT/input_examples/iaron_mediumtoy2.tgf");
-		cases = searchObject.getTestSuite(graph.getRoot());
+		cases = searchObject.getTestSuite(graph.getRoot(), 0);
 		Assert.assertEquals(4, cases.size());
 		
 		graph = tgfReader.getGraph("/ARRSTT/input_examples/iaron_mediumtoy3.tgf");
-		cases = searchObject.getTestSuite(graph.getRoot());
+		cases = searchObject.getTestSuite(graph.getRoot(), 0);
 		Assert.assertEquals(10, cases.size());
 		
 		graph = tgfReader.getGraph("/ARRSTT/input_examples/iaron_mediumtoy4.tgf");
-		cases = searchObject.getTestSuite(graph.getRoot());
+		cases = searchObject.getTestSuite(graph.getRoot(), 0);
 		Assert.assertEquals(7, cases.size());
 		
 		// Difficult toys
 		
 		graph = tgfReader.getGraph("/ARRSTT/input_examples/iaron_difficulttoy1.tgf");
-		cases = searchObject.getTestSuite(graph.getRoot());
+		cases = searchObject.getTestSuite(graph.getRoot(), 0);
 		Assert.assertEquals(4, cases.size());
 		
 		graph = tgfReader.getGraph("/ARRSTT/input_examples/iaron_difficulttoy2.tgf");
-		cases = searchObject.getTestSuite(graph.getRoot());
+		cases = searchObject.getTestSuite(graph.getRoot(), 0);
 		Assert.assertEquals(6, cases.size());
 		
 		graph = tgfReader.getGraph("/ARRSTT/input_examples/iaron_difficulttoy3.tgf");
-		cases = searchObject.getTestSuite(graph.getRoot());
+		cases = searchObject.getTestSuite(graph.getRoot(), 0);
 		Assert.assertEquals(10, cases.size());
 		
 		graph = tgfReader.getGraph("/ARRSTT/input_examples/iaron_difficulttoy4.tgf");
-		cases = searchObject.getTestSuite(graph.getRoot());
+		cases = searchObject.getTestSuite(graph.getRoot(), 0);
 		Assert.assertEquals(12, cases.size());
 		
 		// Real graphs
 		
 		graph = tgfReader.getGraph("/ARRSTT/input_examples/jo_ken_po_exemplo.tgf");
-		cases = searchObject.getTestSuite(graph.getRoot());
+		cases = searchObject.getTestSuite(graph.getRoot(), 0);
 		Assert.assertEquals(9, cases.size());
 		
 		graph = tgfReader.getGraph("/ARRSTT/input_examples/Dark_Souls_2_Chest.tgf");
-		cases = searchObject.getTestSuite(graph.getRoot());
+		cases = searchObject.getTestSuite(graph.getRoot(), 0);
 		Assert.assertEquals(7, cases.size());
 	}
 
