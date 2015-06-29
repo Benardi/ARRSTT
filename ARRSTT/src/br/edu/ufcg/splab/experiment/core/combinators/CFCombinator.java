0d0f380c81577fef10e2b6cad76d827fd3f80687
ArthurEmanuelRodriguesCosta <arthur.costa@ccc.ufcg.edu.br>
2015-06-29 02:38:25 -0300

    The CFCombinator has been refactored and is know using a Iterator

--- ARRSTT/src/br/edu/ufcg/splab/experiment/core/combinators/CFCombinator.java
+++ ARRSTT/src/br/edu/ufcg/splab/experiment/core/combinators/CFCombinator.java
@@ -28,40 +28,6 @@ public class CFCombinator extends AbstractCombinator {
 	public CFCombinator() {
 		super(new ArrayList<InterfaceFactor<?>>());
 	}
-
-    
-	/* There are two things here that we may look... first, this works just for our experiment, because
-	 * it is explicit what each treatment is, and it combines exactly 3 super.getFactors(), I think we can make
-	 * this work for any amount of super.getFactors(). The other thing is that since I've made some changes to Factor
-	 * itself and that now we have a Treatment class, I think this method should return a List<List<Treatment>>
-	 * because this will make it more flexible and keep the Expert. So, what do you think? - Iaron
-	 */
-	 /*
-	public List<List<?>> combine(){
-		List<List<?>> combinatedList = new ArrayList<List<?>>();
-		InterfaceTreatment<?>[] searches = (InterfaceTreatment[]) super.getsuper.getFactors()().get(0).getTreatments();
-		InterfaceTreatment<?>[] loopCoverage = (InterfaceTreatment[]) super.getsuper.getFactors()().get(1).getTreatments();
-		InterfaceTreatment<?>[] factorBranches = (InterfaceTreatment[]) super.getsuper.getFactors()().get(2).getTreatments();
-		
-		
-		for (InterfaceTreatment<?> search : searches) {
-			for (InterfaceTreatment<?>  loop : loopCoverage) {
-				for (InterfaceTreatment<?>  factorBranch : factorBranches) {
-					List<InterfaceTreatment<?>> f = new ArrayList<InterfaceTreatment<?>>();
-					
-					f.add(search);
-					f.add(loop);
-					f.add(factorBranch);
-					
-					combinatedList.add(f);
-				}
-			}
-		}
-		
-	
-		
-		return combinatedList;
-	}*/
 	
 	/**
 	 * Generate all the possible combinations for the treatments.
@@ -106,10 +72,12 @@ public class CFCombinator extends AbstractCombinator {
 	 */
 	
 	private void generateCombinations(List<InterfaceTreatment<?>> combination, InterfaceFactor<?> factor) {
+
 		for (InterfaceTreatment<?> treatment : factor.getTreatments()) {
 			List<InterfaceTreatment<?>> miniList = new ArrayList<InterfaceTreatment<?>>(combination);
 			miniList.add(treatment);
 		}
+		
 	}
 	
 	/**
