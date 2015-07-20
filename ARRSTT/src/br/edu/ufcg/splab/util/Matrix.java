b0a19da4c97d5d05ed444461e2d5c2edcc5ae4c1
linha9 <wesleyoliveirasilva@live.com>
2015-07-19 22:13:27 -0300

    Added another selection method.

--- ARRSTT/src/br/edu/ufcg/splab/util/Matrix.java
+++ ARRSTT/src/br/edu/ufcg/splab/util/Matrix.java
@@ -1,37 +1,9 @@
 package br.edu.ufcg.splab.util;
 
-public class Matrix {
-	private Double[][] innerMatrix;
-	private int colAmount;
-	private int rowAmount;
-	
-	public Matrix(int colAmount, int rowAmount) {
-		this.innerMatrix = new Double[rowAmount][colAmount];
-		this.colAmount = colAmount;
-		this.rowAmount = rowAmount;
-	}
-
-	public void setPos(int row, int col, Double element) {
-		innerMatrix[row][col] = element;
-	}
-
-	public Double get(int row, int col) {
-		return innerMatrix[row][col];
-	}
-
-	public boolean isEmpty() {
-		return (innerMatrix.length > 0) ? (false) : (true);
-	}
-
-	public void remove(int row, int col) {
-		innerMatrix[row][col] = new Double(-1);
-	}
-
-	public int getRowAmount() {
-		return rowAmount;
-	}
-	
-	public int getColAmount() {
-		return colAmount;
-	}
+public interface Matrix {
+	public void setPos(int row, int col, double element);
+	public double get(int row, int col);
+	public boolean isEmpty();
+	public int getRowAmount();
+	public int getColAmount();
 }
