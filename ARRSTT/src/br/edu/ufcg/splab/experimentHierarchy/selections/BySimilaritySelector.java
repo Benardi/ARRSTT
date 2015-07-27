c183af080b8d7da2f7107d389f2fd92f0c3aacaf
linha9 <wesleyoliveirasilva@live.com>
2015-07-27 20:37:57 -0300

    Added the new data structure.

--- ARRSTT/src/br/edu/ufcg/splab/experimentHierarchy/selections/BySimilaritySelector.java
+++ ARRSTT/src/br/edu/ufcg/splab/experimentHierarchy/selections/BySimilaritySelector.java
@@ -2,34 +2,29 @@ package br.edu.ufcg.splab.experimentHierarchy.selections;
 
 import java.util.Random;
 
-import br.edu.ufcg.splab.experimentHierarchy.util.matrix.SelectionMatrix;
-import br.edu.ufcg.splab.experimentHierarchy.util.testcollections.TestCase;
+import br.edu.ufcg.splab.experimentHierarchy.util.matrix.Matrix;
+import br.edu.ufcg.splab.experimentHierarchy.util.matrix.SimilarityStructure;
 import br.edu.ufcg.splab.experimentHierarchy.util.testcollections.TestSuite;
-import br.edu.ufcg.splab.graph.core.InterfaceEdge;
 
 public class BySimilaritySelector {
     private int selectingAmount;
     private TestSuite originalTS;
     private TestSuite selectedTS;
-    private SelectionMatrix matrix;
+    private Matrix matrix;
     
     public BySimilaritySelector(TestSuite ts, Double percentage) {
-        selectingAmount = getAmountOfTestCases(ts, percentage);
+        matrix = new SimilarityStructure(ts);
+        selectingAmount = (int) Math.ceil(originalTS.size() * percentage);
         originalTS = ts;
     }
     
-    private int getAmountOfTestCases(TestSuite ts, Double percentage){
-        return (int) Math.ceil(originalTS.size() * percentage);
-    }
-    
     public TestSuite select() {
-        matrix = buildMatrix();
         selectedTS = new TestSuite(originalTS);
         
         int limitIterations = originalTS.size() - selectingAmount;
         
         for(int i = 0; i < limitIterations; i++) {
-            int removingPosition = chooseRemoval(matrixMaxPos());
+            int removingPosition = chooseRemoval();
             removeFromMatrix(removingPosition);
             selectedTS.nulify(removingPosition);
         }
@@ -38,56 +33,9 @@ public class BySimilaritySelector {
         return selectedTS;
     }
     
-    private SelectionMatrix buildMatrix() {
-        SelectionMatrix matrix = new SelectionMatrix(originalTS.size(), originalTS.size());
+    private int chooseRemoval() {
+        int[] maxPos = matrix.findMaxPos();
         
-        for(int i = 0; i < originalTS.size(); i++ ) {
-            for(int j = 0; j < originalTS.size(); j++ ) {
-                if(i >= j) {
-                    matrix.setPos(i, j, -1.0);
-                } else {
-                    matrix.setPos(i, j, getSimilarity(originalTS.get(i), originalTS.get(j)));
-                }
-            }
-        }
-        
-        return matrix;
-    }
-    
-    private double getSimilarity(TestCase tc1, TestCase tc2) {
-        return calculateNit(tc1, tc2) / ((tc1.size() + tc2.size()) / 2);
-    }
-    
-    private int calculateNit(TestCase tc1, TestCase tc2) {
-        int nitCount = 0;
-        
-        for (InterfaceEdge edge : tc1) {
-            if (tc2.contains(edge)) {
-                nitCount++;
-            }
-        }
-        
-        return nitCount;
-    }
-    
-    private int[] matrixMaxPos(){
-        double max = 0;
-        int[] position = new int[2];
-        
-        for(int i = 0; i < originalTS.size(); i++) {
-            for(int j = i + 1; j < originalTS.size(); j++) {
-                if(matrix.get(i, j) >= max) {
-                    max = matrix.get(i, j);
-                    position[0] = i;
-                    position[1] = j;
-                }
-            }
-        }
-        
-        return position;
-    }
-    
-    private int chooseRemoval(int[] maxPos) {
         if(originalTS.get(maxPos[0]).size() < originalTS.get(maxPos[1]).size()) {
             return maxPos[0];
         } else if(originalTS.get(maxPos[1]).size() < originalTS.get(maxPos[0]).size()) {
