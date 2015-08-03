fbf20160419edcb1fc7cc846db31457d8020f057
Francisco Neto <francisco.neto@computacao.ufcg.edu.br>
2015-08-03 11:58:04 -0300

    Refactoring from meeting in Aug. 3rd.

--- ARRSTT/src/br/edu/ufcg/splab/experiment_hierarchy/Main.java
+++ ARRSTT/src/br/edu/ufcg/splab/experiment_hierarchy/Main.java
@@ -4,30 +4,34 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import br.edu.ufcg.splab.experiment_hierarchy.core.combinators.ExperimentSetUp;
+import br.edu.ufcg.splab.experiment_hierarchy.core.combinators.ExperimentSetUpSearches;
 import br.edu.ufcg.splab.experiment_hierarchy.core.experiment_rep.TeamExperiment;
 import br.edu.ufcg.splab.experiment_hierarchy.core.factors.Factor;
 import br.edu.ufcg.splab.experiment_hierarchy.core.factors.InterfaceFactor;
 import br.edu.ufcg.splab.experiment_hierarchy.core.treatments.ExecutableTreatment;
 import br.edu.ufcg.splab.experiment_hierarchy.searches.BreadthFirstSearch;
 import br.edu.ufcg.splab.experiment_hierarchy.searches.DepthFirstSearch;
+import br.edu.ufcg.splab.experiment_hierarchy.util.Tuple;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		List<InterfaceFactor> list = new ArrayList<InterfaceFactor>();
+		//create factor
 		Factor searchs = new Factor();
 		BreadthFirstSearch breadth = new BreadthFirstSearch();
 		DepthFirstSearch depth = new DepthFirstSearch();
 		searchs.addTreatment((ExecutableTreatment) breadth);
-		list.add(searchs);
-		ExperimentSetUp combinate = new ExperimentSetUp(list);
+		
+		//add factor
+		List<InterfaceFactor> list = new ArrayList<InterfaceFactor>();
+		list.add(searchs);		
+		ExperimentSetUpSearches combinate = new ExperimentSetUpSearches(list);
 
-		List<List<ExecutableTreatment>> combinations = combinate.combine();
+		List<Tuple<ExecutableTreatment>> combinations = combinate.combine();
 
 		int[] loopCoverages = { 3, 1, 2 };
 		TeamExperiment experiment = new TeamExperiment(loopCoverages);
-		Iterator<List<ExecutableTreatment>> itr = combinations.iterator();
+		Iterator<Tuple<ExecutableTreatment>> itr = combinations.iterator();
 		while (itr.hasNext()) {
 			List<ExecutableTreatment> element = (List<ExecutableTreatment>) itr.next();
 			experiment.runExperiment(element);
