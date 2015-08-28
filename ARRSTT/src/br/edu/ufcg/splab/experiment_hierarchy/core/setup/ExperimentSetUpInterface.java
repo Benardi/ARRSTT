3f8a5d612b5b4fc24b406337a2d63cf44053d821
José Benardi de Souza Nunes <jose.nunes@ccc.ufcg.edu.br>
2015-08-28 00:53:48 -0300

    Improvements made on Javadoc

--- ARRSTT/src/br/edu/ufcg/splab/experiment_hierarchy/core/setup/ExperimentSetUpInterface.java
+++ ARRSTT/src/br/edu/ufcg/splab/experiment_hierarchy/core/setup/ExperimentSetUpInterface.java
@@ -5,6 +5,18 @@ import java.util.List;
 import br.edu.ufcg.splab.experiment_hierarchy.core.treatments.ExecutableTreatment;
 import br.edu.ufcg.splab.experiment_hierarchy.util.Tuple;
 
+/**
+ * 
+ * Interface that represents the basic structure expected from an experiment set
+ * up.
+ * 
+ * @author JoséBenardi
+ *
+ */
 public interface ExperimentSetUpInterface {
+	/**
+	 * 
+	 * @return The list of independent variables
+	 */
 	public List<Tuple<ExecutableTreatment>> getIndependentVariables();
 }
--- ARRSTT/src/br/edu/ufcg/splab/experiment_hierarchy/selections/BiggestTestCaseSelector.java
+++ ARRSTT/src/br/edu/ufcg/splab/experiment_hierarchy/selections/BiggestTestCaseSelector.java
@@ -3,13 +3,27 @@ package br.edu.ufcg.splab.experiment_hierarchy.selections;
 import br.edu.ufcg.splab.experiment_hierarchy.util.testcollections.TestCase;
 import br.edu.ufcg.splab.experiment_hierarchy.util.testcollections.TestSuite;
 
+/**
+ * This class represents the selection of a test case using as parameter its
+ * dimension.
+ * 
+ * @author JoséBenardi
+ *
+ */
+public class BiggestTestCaseSelector implements InterfaceTestCaseSelector {
 
-public class BiggestTestCaseSelector implements InterfaceTestCaseSelector{
-	
 	public BiggestTestCaseSelector() {
 
 	}
 
+	/**
+	 * 
+	 * @param testSuite
+	 *            the test suite whose section will be separated
+	 * @param percentage
+	 *            the percentage desired from the test suite
+	 * @return how many will be chosen.
+	 */
 	private int getQuantity(TestSuite testSuite, double percentage) {
 		return (int) Math.ceil(testSuite.size() * percentage);
 	}
@@ -17,28 +31,35 @@ public class BiggestTestCaseSelector implements InterfaceTestCaseSelector{
 	@Override
 	public TestSuite select(TestSuite testSuite, Double percentage) {
 		TestSuite result = new TestSuite();
-		int quantity = getQuantity(testSuite, percentage);		
-		for(int i = 0; i < quantity; i++){
+		int quantity = getQuantity(testSuite, percentage);
+		for (int i = 0; i < quantity; i++) {
 			addBiggestTestCase(result, testSuite);
 		}
 		return result;
 	}
 
+	/**
+	 * 
+	 * @param result
+	 *            the test suite that hold the biggest test cases.
+	 * 
+	 * @param testSuite
+	 *            the test suite from where the biggest will be chosen.
+	 */
 	private void addBiggestTestCase(TestSuite result, TestSuite testSuite) {
 		int maxSize = 0;
 		int maxIndex = -1;
-		for(int i = 0; i < testSuite.size(); i++){
-			if(!result.contains(testSuite.get(i))){
-				if(testSuite.get(i).size() > maxSize){
+		for (int i = 0; i < testSuite.size(); i++) {
+			if (!result.contains(testSuite.get(i))) {
+				if (testSuite.get(i).size() > maxSize) {
 					maxSize = testSuite.get(i).size();
 					maxIndex = i;
 				}
 			}
 		}
-		
-		result.add(new TestCase(testSuite.get(maxIndex)));
-		
-	}
 
+		result.add(new TestCase(testSuite.get(maxIndex)));
+
+	}
 
 }
