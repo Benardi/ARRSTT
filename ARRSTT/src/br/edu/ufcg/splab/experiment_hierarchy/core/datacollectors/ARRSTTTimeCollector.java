3625f0ea563ba2adfe7094e8a4804f14c9523047
Iaron <iaron.araujo@ccc.ufcg.edu.br>
2015-09-22 12:07:04 -0300

    Rewrote the javadoc of the whole datacollectors package. I also rewrote the ExecutableTreatment's javadoc and began the changes in GenerationTreatment.

--- ARRSTT/src/br/edu/ufcg/splab/experiment_hierarchy/core/datacollectors/ARRSTTTimeCollector.java
+++ ARRSTT/src/br/edu/ufcg/splab/experiment_hierarchy/core/datacollectors/ARRSTTTimeCollector.java
@@ -2,10 +2,33 @@ package br.edu.ufcg.splab.experiment_hierarchy.core.datacollectors;
 
 import br.edu.ufcg.splab.experiment_hierarchy.core.treatments.ExecutableTreatment;
 import br.edu.ufcg.splab.experiment_hierarchy.util.Tuple;
-
+/* Change		Author		Date
+ * Creation		Wesley		2015-09-13
+ */
+/**
+ * Objective: Used in the ARRSTT experiments, this class is responsible
+ * for collecting the ExecutableTreatment's execution time.
+ * 
+ * Description of use: An ExecutableTreatment is passed to this class so
+ * it can calculate and save it's execution time.
+ * 
+ * @author Wesley
+ *
+ */
 public class ARRSTTTimeCollector implements DependentVariableCollector {
 
 	@Override
+	/**
+	 * Objective: Calculate the ExecutableTreatment's execution time.
+	 * 
+	 * Example of use: In the ARRSTT Selection Experiment this method receives an ExecutableTreatment
+	 * so it can calculate and save it's execution time in a StringBuffer.
+	 * 
+	 * @param treatment
+	 * 			A tuple of ExecutableTreatments.
+	 * @param content
+	 * 			The StringBuffer used to store the ExecutableTreatments' execution time.
+	 */
 	public void collect(Tuple<ExecutableTreatment> treatment,
 			StringBuffer content) {
 		Long initTime = System.nanoTime();
--- ARRSTT/src/br/edu/ufcg/splab/experiment_hierarchy/core/datacollectors/DependentVariableCollector.java
+++ ARRSTT/src/br/edu/ufcg/splab/experiment_hierarchy/core/datacollectors/DependentVariableCollector.java
@@ -3,6 +3,36 @@ package br.edu.ufcg.splab.experiment_hierarchy.core.datacollectors;
 import br.edu.ufcg.splab.experiment_hierarchy.core.treatments.ExecutableTreatment;
 import br.edu.ufcg.splab.experiment_hierarchy.util.Tuple;
 
+/* Change		Author		Date
+ * Creation		Wesley		2015-09-13
+ */
+/**
+ * Objective: This interface represents a dependent variable collector,
+ * being responsible for the collecting an experiment's dependent variable
+ * and saving it in a StringBuffer.
+ * 
+ * Description of use: It is used in an ExperimentRunner so it can collect
+ * the experiment's dependent variable.
+ * 
+ * @author Wesley
+ *
+ */
 public interface DependentVariableCollector {
+	/**
+	 * Objective: Add the dependent variable data collected from a tuple of
+	 * ExecutableTreatments in a StringBuffer.
+	 * 
+	 * Example of use: In an experiment that is going to check if merge sort is
+	 * quicker than bubble sort for a certain list, a Tuple of ExecutableTratment with
+	 * the merge sort and the list can be passed to this method alongside with an empty
+	 * StringBuffer so this method will write the execution time of the sort in the
+	 * StringBuffer.
+	 * 
+	 * @param treatment
+	 * 			A tuple of ExecutableTreatments that is going to have a dependent variable
+	 * 			collected.
+	 * @param content
+	 * 			The StringBuffer used to store the dependent variable's data.
+	 */
 	public void collect(Tuple<ExecutableTreatment> treatment, StringBuffer content);
 }
