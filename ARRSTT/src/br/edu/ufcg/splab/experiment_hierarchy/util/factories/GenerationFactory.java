d464bafe1334cdf5545d8c794da6576c45605e4d
José Benardi de Souza Nunes <jose.nunes@ccc.ufcg.edu.br>
2015-09-24 23:23:37 -0300

    Updated the factories' javadoc except for the SetUpFactory whose very existence is still not confirmed.

--- ARRSTT/src/br/edu/ufcg/splab/experiment_hierarchy/util/factories/GenerationFactory.java
+++ ARRSTT/src/br/edu/ufcg/splab/experiment_hierarchy/util/factories/GenerationFactory.java
@@ -5,8 +5,20 @@ import br.edu.ufcg.splab.experiment_hierarchy.searches.DepthFirstSearch;
 import br.edu.ufcg.splab.experiment_hierarchy.searches.InterfaceSearch;
 import br.edu.ufcg.splab.experiment_hierarchy.util.enums.GenerationType;
 
+/**
+ * Objective: This class covers all necessary procedure involved in the process
+ * of generating a search.
+ * 
+ * Description of use: Receives a type and returns a search of the respective
+ * type.
+ */
 public class GenerationFactory {
-	
+	/**
+	 * 
+	 * @param type
+	 *            the type of search
+	 * @return A search
+	 */
 	public InterfaceSearch createTreatment(GenerationType type) {
 		if (type == GenerationType.BFS) {
 			return createBfsGenerator();
@@ -14,11 +26,17 @@ public class GenerationFactory {
 			return createDfsGenerator();
 		}
 	}
-	
+	/**
+	 * 
+	 * @return A Breadth First Search.
+	 */
 	public InterfaceSearch createBfsGenerator() {
 		return new BreadthFirstSearch();
 	}
-	
+	/**
+	 * 
+	 * @return A Depth First Search.
+	 */
 	public InterfaceSearch createDfsGenerator() {
 		return new DepthFirstSearch();
 	}
--- ARRSTT/src/br/edu/ufcg/splab/experiment_hierarchy/util/factories/TreatmentFactory.java
+++ ARRSTT/src/br/edu/ufcg/splab/experiment_hierarchy/util/factories/TreatmentFactory.java
@@ -10,16 +10,47 @@ import br.edu.ufcg.splab.experiment_hierarchy.util.enums.SelectionType;
 import br.edu.ufcg.splab.experiment_hierarchy.util.testcollections.TestSuite;
 import br.edu.ufcg.splab.graph_hierarchy.core.InterfaceVertex;
 
+/**
+ * Objective: This class covers all necessary procedure involved in the process
+ * of generating a treatment.
+ * 
+ * Description of use: This class is used in the "set up" type classes to where
+ * for e.g it can be used to generate the independent variables.
+ */
 public class TreatmentFactory {
-	public ExecutableTreatment createSelection(SelectionType type, TestSuite testSuite, double percentage) {
-		//cria o selection type
-		// chamar o selection para retornar a instancia de selection. 
-		InterfaceTestCaseSelector selector = new SelectionFactory().createTreatment(type);
+	/**
+	 * 
+	 * @param type
+	 *            The type of selection
+	 * @param testSuite
+	 *            the test suite.
+	 * @param percentage
+	 *            the percentage
+	 * @return A selection treatment that is composed by all the parameters.
+	 */
+	public ExecutableTreatment createSelection(SelectionType type,
+			TestSuite testSuite, double percentage) {
+		// cria o selection type
+		// chamar o selection para retornar a instancia de selection.
+		InterfaceTestCaseSelector selector = new SelectionFactory()
+				.createTreatment(type);
 		return new SelectionTreatment(selector, testSuite, percentage);
 	}
-	
-	public ExecutableTreatment createGeneration(GenerationType type, InterfaceVertex root, int loopCoverage) {
-		InterfaceSearch generator = new GenerationFactory().createTreatment(type);		
+
+	/**
+	 * 
+	 * @param type
+	 *            the type of generation
+	 * @param root
+	 *            the root
+	 * @param loopCoverage
+	 *            the loop coverage
+	 * @return A generation treatment that is composed by all the parameters.
+	 */
+	public ExecutableTreatment createGeneration(GenerationType type,
+			InterfaceVertex root, int loopCoverage) {
+		InterfaceSearch generator = new GenerationFactory()
+				.createTreatment(type);
 		return new GenerationTreatment(generator, root, loopCoverage);
 	}
 }
