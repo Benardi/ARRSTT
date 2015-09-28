d06f50d83f93bfd8bb1bc245d2b0eebcc81cac83
SILVAWesley <wesley.silva@ccc.ufcg.edu.br>
2015-09-28 09:03:52 -0300

    Parametrized the ExperimentFactory.

--- ARRSTT/src/br/edu/ufcg/splab/experiment_hierarchy/Main.java
+++ ARRSTT/src/br/edu/ufcg/splab/experiment_hierarchy/Main.java
@@ -1,13 +1,21 @@
 package br.edu.ufcg.splab.experiment_hierarchy;
 
 import br.edu.ufcg.splab.experiment_hierarchy.core.experiments.Experiment;
+import br.edu.ufcg.splab.experiment_hierarchy.util.enums.GenerationType;
+import br.edu.ufcg.splab.experiment_hierarchy.util.enums.SelectionType;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
 		ExperimentFactory factory = new ExperimentFactory();
 		try {
-			Experiment experiment = factory.buildArrsttGeneration();
+			int[] loopCoverages = { 1, 4, 7 };
+			GenerationType[] generationAlgorithms = {GenerationType.BFS, GenerationType.DFS};
+			Experiment experiment = factory.buildGeneration(loopCoverages, generationAlgorithms);
+			experiment.execute();
+			
+			SelectionType[] selectionAlgorithms = {SelectionType.BIGGEST, SelectionType.SIMILARITY, SelectionType.RANDOMIZED};
+			experiment = factory.buildSelection(selectionAlgorithms);
 			experiment.execute();
 		} catch(Exception e) {
 			e.printStackTrace();
