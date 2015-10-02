825c0b8d176001b22c5021f9a6d728b4c94fa3f4
José Benardi de Souza Nunes <jose.nunes@ccc.ufcg.edu.br>
2015-10-01 22:55:56 -0300

    The selection related classes had their Javadoc updated and standardized.

--- ARRSTT/src/br/edu/ufcg/splab/experiment_hierarchy/selections/InterfaceTestCaseSelector.java
+++ ARRSTT/src/br/edu/ufcg/splab/experiment_hierarchy/selections/InterfaceTestCaseSelector.java
@@ -3,13 +3,22 @@ package br.edu.ufcg.splab.experiment_hierarchy.selections;
 import br.edu.ufcg.splab.experiment_hierarchy.util.testcollections.TestSuite;
 
 /**
- * This interface represents what a selector should be capable of doing.
+ * Objective: This interface represents what a selector should be capable of
+ * doing.
  * 
- * @author JoséBenardi
+ * Description of use: Every and each selector of this working set implements
+ * this interface and can be declared as one.
  *
  */
 public interface InterfaceTestCaseSelector {
 	/**
+	 * Objective: Generate a smaller test suite from the test cases selected
+	 * from a previous through some method.
+	 * 
+	 * Description of use: If a test suite is selected through the biggest case
+	 * selector with 0.5 percentage, half of the previous test suite will be in
+	 * the new one and this new one will be filled by the biggest test case from
+	 * the other.
 	 * 
 	 * @param testSuite
 	 *            the original test suite
--- ARRSTT/src/br/edu/ufcg/splab/experiment_hierarchy/util/SimilarityCalculator.java
+++ ARRSTT/src/br/edu/ufcg/splab/experiment_hierarchy/util/SimilarityCalculator.java
@@ -4,31 +4,51 @@ import br.edu.ufcg.splab.experiment_hierarchy.util.testcollections.TestCase;
 import br.edu.ufcg.splab.graph_hierarchy.core.InterfaceEdge;
 
 /**
- * The selection by similarity case needs a calculator of similarities,
- * this class takes care of these calculations. 
+ * Objective: Used in the Selection by Similarity class, this class is
+ * responsible for the calculation of similarities.
+ * 
+ * Description of use: Two test cases are passed to this class so it can
+ * calculate how many similarities are between both and return it as a number.
  *
  */
 public class SimilarityCalculator {
 	/**
-	 * Calculates the similarity between two given test cases.
+	 * Objective: Measures the similarity between two given test cases.
 	 * 
-	 * @param tc1 The first test case.
-	 * @param tc2 The second test case.
+	 * Description of use: Receives two test cases and generates a value that
+	 * represent how similar they are.
+	 * 
+	 * @param tc1
+	 *            The first test case.
+	 * @param tc2
+	 *            The second test case.
 	 * @return The similarity between tc1 and tc2.
 	 */
-    public double getSimilarity(TestCase tc1, TestCase tc2) {
-        return calculateNit(tc1, tc2) / ((tc1.size() + tc2.size()) / 2.0);
-    }
-    
-    private int calculateNit(TestCase tc1, TestCase tc2) {
-        int nitCount = 0;
-        
-        for (InterfaceEdge edge : tc1) {
-            if (tc2.contains(edge)) {
-                nitCount++;
-            }
-        }
-        
-        return nitCount;
-    }
+	public double getSimilarity(TestCase tc1, TestCase tc2) {
+		return calculateNit(tc1, tc2) / ((tc1.size() + tc2.size()) / 2.0);
+	}
+
+	/**
+	 * Objective: Measure how many elements are shared between two test cases.
+	 * 
+	 * Description of use: Used by the method getSimilarity as part of its
+	 * calculus.
+	 * 
+	 * @param tc1
+	 *            The first test case.
+	 * @param tc2
+	 *            The second test case.
+	 * @return The number of elements shared between tc1 and tc2.
+	 */
+	private int calculateNit(TestCase tc1, TestCase tc2) {
+		int nitCount = 0;
+
+		for (InterfaceEdge edge : tc1) {
+			if (tc2.contains(edge)) {
+				nitCount++;
+			}
+		}
+
+		return nitCount;
+	}
 }
