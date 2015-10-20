4001c70fcddaa54ca37a3d48d39b00410f6d56c0
Iaron <iaron.araujo@ccc.ufcg.edu.br>
2015-10-20 20:32:23 -0200

    Transfered the logic in the façade to the controller and documented the controller.

--- ARRSTT/src/br/edu/ufcg/splab/experiment_hierarchy/facade/ARRSTTFacade.java
+++ ARRSTT/src/br/edu/ufcg/splab/experiment_hierarchy/facade/ARRSTTFacade.java
@@ -6,24 +6,12 @@ import java.util.List;
  * Change														Author				Date
  * -------------------------------------------------------------------------------------------
  * Creation														Wesley Silva		2015-09-30
- * 
+ * Transfered it's logic to the controller						Iaron Araújo		2015-10-20
  */
 
 public class ARRSTTFacade {
 	public void execute(List<List<String>> inputs) throws Exception {
 		ARRSTTController controller = new ARRSTTController();
-		
-		switch(inputs.get(0).get(0).toLowerCase()) {
-			case "generation":
-				controller.buildGeneration(inputs);
-				break;
-			case "selection":
-				controller.buildSelection(inputs);
-				break;
-			default:
-				throw new Exception("Invalid experiment type.");
-		}
-		
-		controller.executeExperiment();
+		controller.execute(inputs);
 	}
 }
