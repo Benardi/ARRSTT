daa827484bb6a31c886c2ae16843be5bf75a9fa3
linha9 <wesleyoliveirasilva@live.com>
2015-10-26 17:39:47 -0300

    Started to implement ReqTracerAllTransitionsCoverage and GreedyStructure.

---
+++ ARRSTT/src/br/edu/ufcg/splab/experiment_hierarchy/tests/TestReqTracer.java
@@ -0,0 +1,27 @@
+package br.edu.ufcg.splab.experiment_hierarchy.tests;
+
+import br.edu.ufcg.splab.experiment_hierarchy.minimizations.requirements.GreedyStructure;
+import br.edu.ufcg.splab.experiment_hierarchy.minimizations.requirements.ReqTracerAllTransitionsCoverage;
+import br.edu.ufcg.splab.experiment_hierarchy.searches.DepthFirstSearch;
+import br.edu.ufcg.splab.experiment_hierarchy.searches.InterfaceSearch;
+import br.edu.ufcg.splab.experiment_hierarchy.util.testcollections.TestSuite;
+import br.edu.ufcg.splab.graph_hierarchy.core.InterfaceGraph;
+import br.edu.ufcg.splab.graph_hierarchy.parser.ReadTGF;
+
+public class TestReqTracer {
+	public static void main(String[] args) {
+		try {
+			ReadTGF reader = new ReadTGF();
+			InterfaceGraph graph = reader.getGraph("C:\\Users\\Wesley\\git\\application-of-reproducibility-research-with-software-testing\\ARRSTT\\input_examples\\iaron_easytoy4.tgf");
+			InterfaceSearch search = new DepthFirstSearch();
+			TestSuite ts = search.getTestSuite(graph.getRoot(), 0);
+			
+			ReqTracerAllTransitionsCoverage coverager = new ReqTracerAllTransitionsCoverage(ts);
+			GreedyStructure gs = new GreedyStructure(coverager.getMap());
+			
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
