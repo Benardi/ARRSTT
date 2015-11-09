cfef456fb4c8781591a4635712fbea8a3163678d
SILVAWesley <wesley.silva@ccc.ufcg.edu.br>
2015-11-09 20:18:59 -0300

    Begun the implementation of the new minimization design. Implemented: 	- The new structure; 	- Interface for minimization techniques; 	- A tuple representation.

---
+++ ARRSTT/src/br/edu/ufcg/splab/experiment_hierarchy/minimizations/structures/InterfaceMinimizationTechnique.java
@@ -0,0 +1,7 @@
+package br.edu.ufcg.splab.experiment_hierarchy.minimizations.structures;
+
+import br.edu.ufcg.splab.experiment_hierarchy.util.testcollections.TestCase;
+
+public interface InterfaceMinimizationTechnique {
+	public TestCase minimize();
+}
