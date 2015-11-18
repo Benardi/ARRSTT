f31a3c111232e513ab8555302da425afe3995c68
linha9 <wesleyoliveirasilva@live.com>
2015-11-18 12:21:18 -0300

    Implemented Harrold technique. Overloaded the getRandomTestCase method on Randomizer.

--- ARRSTT/src/br/edu/ufcg/splab/experiment_hierarchy/util/Randomizer.java
+++ ARRSTT/src/br/edu/ufcg/splab/experiment_hierarchy/util/Randomizer.java
@@ -1,7 +1,9 @@
 package br.edu.ufcg.splab.experiment_hierarchy.util;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
 
 import br.edu.ufcg.splab.experiment_hierarchy.util.testcollections.TestCase;
 
@@ -10,8 +12,19 @@ public class Randomizer {
 	public static TestCase getRandomTestCase(List<TestCase> tCases){
 		Random r = new Random();
 		int n = r.nextInt(tCases.size());
-		return tCases.get(n);
+		return (!tCases.isEmpty()) ? (tCases.get(n)) : (null);
 	}
 	
-	
+	public static TestCase getRandomTestCase(Set<TestCase> tCases) {
+		Random r = new Random();
+		int n = r.nextInt(tCases.size());
+		
+		Iterator<TestCase> iterator = tCases.iterator();
+		
+		for (int i = 0; i < n; i++) {
+			iterator.next();
+		}
+		
+		return (iterator.hasNext()) ? (iterator.next()) : (null);
+	}
 }
