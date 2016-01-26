75501a309c3c88e3f1f12e163a850edd8386e51d
linha9 <wesleyoliveirasilva@live.com>
2016-01-26 15:12:54 -0300

    The parser is now complete (not refactored version).

--- ARRSTT/src/br/edu/ufcg/splab/parsers/Tester.java
+++ ARRSTT/src/br/edu/ufcg/splab/parsers/Tester.java
@@ -3,7 +3,7 @@ package br.edu.ufcg.splab.parsers;
 public class Tester {
 	public static void main(String[] args) {
 		EvosuiteParser parser = new EvosuiteParser();
-		parser.parseFile("C:/Users/Wesley/git/application-of-reproducibility-research-with-software-testing/ARRSTT/src/br/edu/ufcg/splab/experiment_hierarchy/core/datacollectors/ARRSTTDefectiveEdgesCollector.java");
+		parser.parseFile("C:/Users/Wesley/git/application-of-reproducibility-research-with-software-testing/ARRSTT/src/br/edu/ufcg/splab/experiment_hierarchy/tests/SearchsByPathSizeTestCase.java");
 		
 	}
 }
