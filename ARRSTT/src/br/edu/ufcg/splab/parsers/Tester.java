9a5671e97b3b7993aa6feb9009479927c6ebd291
linha9 <wesleyoliveirasilva@live.com>
2016-01-26 17:05:48 -0300

    About EvosuitParser: Fixed some bugs and refactored (No handling on try/catch yet).

--- ARRSTT/src/br/edu/ufcg/splab/parsers/Tester.java
+++ ARRSTT/src/br/edu/ufcg/splab/parsers/Tester.java
@@ -3,7 +3,6 @@ package br.edu.ufcg.splab.parsers;
 public class Tester {
 	public static void main(String[] args) {
 		EvosuiteParser parser = new EvosuiteParser();
-		parser.parseFile("C:/Users/Wesley/git/application-of-reproducibility-research-with-software-testing/ARRSTT/src/br/edu/ufcg/splab/experiment_hierarchy/tests/SearchsByPathSizeTestCase.java");
-		
+		parser.parseFile("");
 	}
 }
