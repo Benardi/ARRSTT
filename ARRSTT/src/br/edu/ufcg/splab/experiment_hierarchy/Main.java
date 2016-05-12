7862dc4f2d085ced0adeab9399dcbbc5cf42be43
SILVAWesley <wesley.silva@ccc.ufcg.edu.br>
2016-05-12 20:10:04 -0300

    Made ARRESTT work again.

--- ARRSTT/src/br/edu/ufcg/splab/experiment_hierarchy/Main.java
+++ ARRSTT/src/br/edu/ufcg/splab/experiment_hierarchy/Main.java
@@ -1,7 +1,12 @@
 package br.edu.ufcg.splab.experiment_hierarchy;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import br.edu.ufcg.splab.experiment_hierarchy.core.experiments.Experiment;
-import br.edu.ufcg.splab.experiment_hierarchy.util.enums.GenerationType;
+import br.edu.ufcg.splab.experiment_hierarchy.util.enums.DVCType;
 import br.edu.ufcg.splab.experiment_hierarchy.util.enums.SelectionType;
 
 public class Main {
@@ -14,9 +19,18 @@ public class Main {
 			//Experiment experiment = factory.buildGeneration(loopCoverages, generationAlgorithms);
 			//experiment.execute();
 			
-			SelectionType[] selectionAlgorithms = {SelectionType.BIGGEST, SelectionType.SIMILARITY, SelectionType.RANDOMIZED};
-			//Experiment experiment = factory.buildSelection(selectionAlgorithms, 0.5);
-			//experiment.execute();
+			//SelectionType[] selectionAlgorithms = {SelectionType.BIGGEST, SelectionType.SIMILARITY, SelectionType.RANDOMIZED};
+			List<SelectionType> selectionTechniques = new ArrayList<SelectionType>();
+			selectionTechniques.add(SelectionType.BIGGEST);
+			selectionTechniques.add(SelectionType.SIMILARITY);
+			selectionTechniques.add(SelectionType.RANDOMIZED);
+			
+			DVCType[] dvcs = {DVCType.FAILURES, DVCType.DEFECTS, DVCType.DEFECTIVE_EDGES, DVCType.SIZE};
+			List<DVCType> dvcsForSelection = new ArrayList<DVCType>(Arrays.asList(dvcs));
+			
+			
+			Experiment experiment = factory.buildSelection(selectionTechniques, 1, dvcsForSelection);
+			experiment.execute();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
