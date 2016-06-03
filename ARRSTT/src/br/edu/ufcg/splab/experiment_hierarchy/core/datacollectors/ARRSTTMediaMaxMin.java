c458b93db6ddff25a3f5291406dfa8aea1661ca8
Iaron da Costa Araujo <iaron.araujo@ccc.ufcg.edu.br>
2016-06-03 15:07:19 -0300

    - Created the TestSuite merger and three more DVCs

---
+++ ARRSTT/src/br/edu/ufcg/splab/experiment_hierarchy/core/datacollectors/ARRSTTMediaMaxMin.java
@@ -0,0 +1,35 @@
+package br.edu.ufcg.splab.experiment_hierarchy.core.datacollectors;
+
+import br.edu.ufcg.splab.experiment_hierarchy.util.testcollections.TestCase;
+import br.edu.ufcg.splab.experiment_hierarchy.util.testcollections.TestSuite;
+import br.edu.ufcg.splab.gambiarra.MediaMaxMin;
+
+public class ARRSTTMediaMaxMin implements DependentVariableCollector{
+
+	@Override
+	public StringBuffer collect(TestSuite testSuite) {
+		MediaMaxMin result = calculate(testSuite);
+		return new StringBuffer("Media: " + result.getMedia()+ "Max: " + result.getMax() + "Min: " + result.getMin());
+	}
+	
+	private MediaMaxMin calculate(TestSuite ts){
+		int min, max, tcsize, sum;
+		min = Integer.MAX_VALUE;
+		max = Integer.MIN_VALUE;
+		sum = 0;
+		
+		
+		for (TestCase tc : ts.getTestSuite()) {
+			tcsize = tc.size();
+			if (tcsize < min)
+				min = tcsize;
+			if (tcsize > max)
+				max = tcsize;
+			sum += tcsize;
+		}
+		double media = (1.0 * sum) / ts.size();
+		
+		return new MediaMaxMin(media, max, min);
+	}
+
+}
