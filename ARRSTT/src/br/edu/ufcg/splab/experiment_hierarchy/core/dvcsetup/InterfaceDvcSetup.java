3e1c26ccac15c48d149223129e65e914e76dfff7
linha9 <wesleyoliveirasilva@live.com>
2016-06-15 15:18:41 -0300

    - Started the dvc solution.

---
+++ ARRSTT/src/br/edu/ufcg/splab/experiment_hierarchy/core/dvcsetup/InterfaceDvcSetup.java
@@ -0,0 +1,5 @@
+package br.edu.ufcg.splab.experiment_hierarchy.core.dvcsetup;
+
+public interface InterfaceDvcSetup {
+
+}
--- ARRSTT/src/br/edu/ufcg/splab/experiment_hierarchy/core/runners/TestRunner.java
+++ ARRSTT/src/br/edu/ufcg/splab/experiment_hierarchy/core/runners/TestRunner.java
@@ -2,20 +2,24 @@ package br.edu.ufcg.splab.experiment_hierarchy.core.runners;
 
 import java.util.List;
 
+import br.edu.ufcg.splab.experiment_hierarchy.core.artifacts.TestArtifact;
 import br.edu.ufcg.splab.experiment_hierarchy.core.treatments.ExecutableTreatment;
 import br.edu.ufcg.splab.experiment_hierarchy.util.Tuple;
 
 public class TestRunner implements InterfaceRunner {
+	private List<TestArtifact> artifacts;
 	
-	public TestRunner(List<DependentVariableCollector> ) {
-		
+	public TestRunner(List<TestArtifact> artifacts) {
+		this.artifacts  = artifacts;
 	}
 	
 	
 	@Override
 	public void runExperiment(List<Tuple<ExecutableTreatment>> combinations) {
-		// TODO Auto-generated method stub
-		
+		for (int i = 0; i < combinations.size(); i++) {
+			
+			
+		}
 	}
 
 }
