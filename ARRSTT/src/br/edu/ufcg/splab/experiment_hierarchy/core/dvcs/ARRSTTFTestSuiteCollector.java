9b1bd7c6e4a7d84f9491c147df6966c28cf2788b
SILVAWesley <wesley.silva@ccc.ufcg.edu.br>
2016-06-18 23:33:37 -0300

    - Something went wrong with the earlier commit.

--- ARRSTT/src/br/edu/ufcg/splab/experiment_hierarchy/core/dvcs/ARRSTTFTestSuiteCollector.java
+++ ARRSTT/src/br/edu/ufcg/splab/experiment_hierarchy/core/dvcs/ARRSTTFTestSuiteCollector.java
@@ -3,7 +3,7 @@ package br.edu.ufcg.splab.experiment_hierarchy.core.dvcs;
 import br.edu.ufcg.splab.experiment_hierarchy.core.api.InterfaceDvc;
 import br.edu.ufcg.splab.experiment_hierarchy.util.testcollections.TestSuite;
 
-public class FinalTestSuiteCollector implements InterfaceDvc {
+public class ARRSTTFTestSuiteCollector implements InterfaceDvc {
 
 	@Override
 	public StringBuffer collect(TestSuite t) {
